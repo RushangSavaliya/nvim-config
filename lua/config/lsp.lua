bf2b61fdcd4ae4c73c0a2f1bf11f88d02d06e43e
RushangSavaliya <rushangsavaliya19@gmail.com>
2026-07-25 23:41:38 +0530

    setup python LSP

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -25,6 +25,29 @@ vim.lsp.config("lua_ls", {
 
 vim.lsp.enable("lua_ls")
 
+vim.lsp.config("pyright", {
+	cmd = { "pyright-langserver", "--stdio" },
+	filetypes = { "python" },
+
+	root_markers = {
+		"pyproject.toml",
+		"setup.py",
+		"setup.cfg",
+		"requirements.txt",
+		".git",
+	},
+
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "basic",
+			},
+		},
+	},
+})
+
+vim.lsp.enable("pyright")
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
