929a1ca91d1e35c7e732254ebe631f70e3809b19
user.email <rushangsavaliya19@gmail.com>
2026-08-15 11:03:18 +0530

    center cursor on ctrl-d/u and update neoscroll scroll API

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,12 +1,7 @@
 return {
 	"karb94/neoscroll.nvim",
 	opts = {
-		mappings = {
-			"<C-u>",
-			"<C-d>",
-			"<C-b>",
-			"<C-f>",
-		},
+		mappings = {},
 
 		hide_cursor = true,
 		stop_eof = true,
@@ -18,4 +13,40 @@ return {
 
 		performance_mode = false,
 	},
+	config = function(_, opts)
+		local neoscroll = require("neoscroll")
+		neoscroll.setup(opts)
+
+		local function scroll(amount)
+			return function()
+				neoscroll.scroll(amount, { move_cursor = true, duration = 100 })
+			end
+		end
+
+		local function center()
+			vim.cmd("normal! zz")
+		end
+
+		vim.keymap.set("n", "<C-u>", function()
+			neoscroll.scroll(-vim.wo.scroll, { move_cursor = true, duration = 100 })
+			center()
+		end, { desc = "Scroll up half page (smooth, centered)" })
+		vim.keymap.set("n", "<C-d>", function()
+			neoscroll.scroll(vim.wo.scroll, { move_cursor = true, duration = 100 })
+			center()
+		end, { desc = "Scroll down half page (smooth, centered)" })
+
+		vim.keymap.set({ "v", "x" }, "<C-u>", scroll(-vim.wo.scroll), { desc = "Scroll up half page (smooth)" })
+		vim.keymap.set({ "v", "x" }, "<C-d>", scroll(vim.wo.scroll), { desc = "Scroll down half page (smooth)" })
+		local function win_height()
+			return vim.api.nvim_win_get_height(0)
+		end
+
+		vim.keymap.set({ "n", "v", "x" }, "<C-b>", function()
+			neoscroll.scroll(-win_height(), { move_cursor = true, duration = 450 })
+		end, { desc = "Scroll up full page (smooth)" })
+		vim.keymap.set({ "n", "v", "x" }, "<C-f>", function()
+			neoscroll.scroll(win_height(), { move_cursor = true, duration = 450 })
+		end, { desc = "Scroll down full page (smooth)" })
+	end,
 }
