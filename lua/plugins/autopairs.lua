5d294d1914bde4f0b9e5a575543c580325d300bb
user.email <rushangsavaliya19@gmail.com>
2026-08-17 22:00:36 +0530

    add nvim-autopairs for auto bracket/quote pairing

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,14 @@
+return {
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	opts = {
+		check_ts = true,
+		ts_config = {
+			lua = { "string" },
+			javascript = { "template_string" },
+			java = false,
+		},
+		enable_check_bracket_line = true,
+		ignored_next_char = "[%w%%%'%[%\"%.%`%$]",
+	},
+}
